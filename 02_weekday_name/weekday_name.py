f483dced4d9f2c307d2d40786e568b6593d8d098
KendrickPC <kennethpchang@gmail.com>
2022-06-21 15:38:47 -0700

    weekday_name.py complete

--- 02_weekday_name/weekday_name.py
+++ 02_weekday_name/weekday_name.py
@@ -1,3 +1,6 @@
+# https://stackoverflow.com/questions/11479816/what-is-the-python-equivalent-for-a-case-switch-statement
+# In Python 3.10, they introduced the pattern matching.
+
 def weekday_name(day_of_week):
     """Return name of weekday.
     
@@ -12,3 +15,30 @@ def weekday_name(day_of_week):
         >>> weekday_name(9)
         >>> weekday_name(0)
     """
+    if day_of_week == 1:
+        return "Sunday"
+    elif day_of_week == 2:
+        return "Monday"
+    elif day_of_week == 3:
+        return "Tuesday"
+    elif day_of_week == 4:
+        return "Wednesday"
+    elif day_of_week == 5:
+        return "Thursday"
+    elif day_of_week == 6:
+        return "Friday"
+    elif day_of_week == 7:
+        return "Saturday"
+    else:
+        return None
+
+print(weekday_name(1))
+print(weekday_name(2))
+print(weekday_name(3))
+print(weekday_name(4))
+print(weekday_name(5))
+print(weekday_name(6))
+print(weekday_name(7))
+
+print(weekday_name(9))
+print(weekday_name(0))
